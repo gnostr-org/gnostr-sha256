b63657108653141d9e2f42273e2bdba78796113f
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-07-29 09:44:19 -0400

    src/lib.rs: src/main.rs: cargo test byte queries

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,49 @@ use std::process;
 pub struct Config {
     pub query: String,
 }
+
+pub fn print_type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
+
 impl Config {
+
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() == 1 {
+
             let query = digest("");
-            println!("{}", query);
+
+            #[cfg(debug_assertions)]
+            let cloned_query = query.clone();
+            #[cfg(debug_assertions)]
+            println!("{:?}", print_type_of(&query));
+            #[cfg(debug_assertions)]
+            println!("{:?}", print_type_of(&cloned_query));
+            println!("{:?}", query);
+            #[cfg(debug_assertions)]
+            let s = &"hello world".to_string();
+            #[cfg(debug_assertions)]
+            let cloned_s = s.clone();
+            #[cfg(debug_assertions)]
+            println!("{:?}", print_type_of(&s));
+            #[cfg(debug_assertions)]
+            println!("{:?}", print_type_of(&cloned_s));
+
+
             process::exit(0);
 
             //return Err("not enough arguments");
         }
 
         let query = args[1].clone();
+        #[cfg(debug_assertions)]
+        let s = &"hello world".to_string();
+        #[cfg(debug_assertions)]
+        let cloned_s = s.clone();
+        #[cfg(debug_assertions)]
+        println!("{:?}", print_type_of(&s));
+        #[cfg(debug_assertions)]
+        println!("{:?}", print_type_of(&cloned_s));
 
         Ok(Config { query })
     }
@@ -29,8 +61,11 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 let mut results = Vec::new();
     for line in contents.lines() {
         // do something with line
+        println!("{}", line);
         if line.contains(query) {
             // do something with line
+            let val = digest(query);
+            println!("{}", val);
             results.push(line);
         }
     }
@@ -50,5 +85,22 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn one_query() {
+        let query = digest("");
+        let contents = "\
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+
+        assert_eq!(vec!["e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"], search(&query, contents));
+    }
+    #[test]
+    #[should_panic]
+    fn panic_query() {
+        let query = digest("");
+        let contents = "\
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 ";
+
+        assert_eq!(vec!["e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"], search(&query, contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,13 @@ fn example() {
     //println!("cwd={:?}",get_current_working_dir());
 
 }
+fn strip_trailing_newline(input: &str) -> &str {
+    input
+        .strip_suffix("\r\n")
+        .or(input.strip_suffix("\n"))
+        .unwrap_or(input)
+}
+
 
 fn main() -> Result<()> {
 
@@ -88,15 +95,88 @@ let config = Config::build(&args).unwrap_or_else(|_err| {
     process::exit(0);
 });
 
-//println!("{}", config.query);
+println!("{}", strip_trailing_newline(&config.query));
+println!("{}", config.query);
 
 if let Err(e) = gnostr_sha256::run(config) {
     println!("Application error: {e}");
     process::exit(1);
 }
 
-
-
 let _duration = time::get_time() - start;
 Ok(())
 }//end main
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sha256::{digest};
+
+
+#[test]
+fn strip_newline_works(){
+    assert_eq!(strip_trailing_newline("Test0\r\n\r\n"), "Test0\r\n");
+    assert_eq!(strip_trailing_newline("Test1\r\n"), "Test1");
+    assert_eq!(strip_trailing_newline("Test2\n"), "Test2");
+    assert_eq!(strip_trailing_newline("Test3"), "Test3");
+}
+#[test]
+fn empty_query() {
+    let query = digest("");
+    let contents = "\
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+    assert_eq!(strip_trailing_newline(&query), contents);
+}
+#[test]
+fn hello_query() {
+    let query = digest("hello");
+    let contents = "\
+2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+assert_eq!(strip_trailing_newline(&query), contents);
+}
+#[test]
+fn raw_byte_hello_query() {
+    let query = digest(r#"hello"#);
+    //let query = digest("hello");
+    let contents = "\
+2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+assert_eq!(strip_trailing_newline(&query), contents);
+}
+#[test]
+fn byte_str_hello_query() {
+    let query = digest(b"hello");
+    let contents = "\
+2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+assert_eq!(strip_trailing_newline(&query), contents);
+}
+#[test]
+fn byte_query() {
+    let query = digest(b"h");
+    let contents = "\
+aaa9402664f1a41f40ebbc52c9993eb66aeb366602958fdfaa283b71e64db123";
+assert_eq!(strip_trailing_newline(&query), contents);
+}
+#[test]
+fn raw_byte_query() {
+    let query = digest(br#"hello"#);
+    let contents = "\
+2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+assert_eq!(strip_trailing_newline(&query), contents);
+}
+#[test]
+#[should_panic]
+fn hello_panic_query() {
+    let query = digest(r#"hello\n"#);
+    let contents = "\
+2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824 ";
+assert_eq!(strip_trailing_newline(&query), contents);
+}
+#[test]
+#[should_panic]
+fn panic_query() {
+    let query = digest("");
+    let contents = "\
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 ";
+
+    assert_eq!(strip_trailing_newline(&query), contents);
+}
+}
